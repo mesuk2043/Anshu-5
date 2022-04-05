f43334f67718c84d0662219270b5ae06ad4aacc8
jamesdarke <98935542+jamesdarke@users.noreply.github.com>
2022-04-06 00:06:21 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -63,8 +63,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/harshpreets63/Mirror-Bot")
-    buttons.buildbutton("Channel", "https://t.me/HarshMirror")
+    buttons.buildbutton("Movies Group", "https://t.me/MOVIES_AND_SERIES_REQUESTING")
+    buttons.buildbutton("Updates Channel", "https://t.me/INDIAHDM0VIES")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
