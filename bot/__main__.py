8ff7218cb8b74e5b14c6b8bcd29a3e4a662214cb
jamesdarke <98935542+jamesdarke@users.noreply.github.com>
2022-04-06 09:33:29 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -68,13 +68,13 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-src="./LOCAL/Kamlimere.jpg"\nThis bot can mirror all your links to Google Drive!
+./LOCAL/Kamlimere.jpg This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
-            '<img src="./LOCAL/Kamlimere.jpg"> \nOops! not a Authorized user.\nPlease join our Channel.\nOr Host Your Own Bot Using My Repo.',
+            './LOCAL/Kamlimere.jpg Oops! not a Authorized user.\nPlease join our Channel.\nOr Host Your Own Bot Using My Repo.',
             context.bot,
             update,
             reply_markup,
