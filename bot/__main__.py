6a3599795c34a4dbff2f084a925dba0a4fb3adea
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-04-08 19:22:50 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from bot.helper.ext_utils.telegraph_helper import telegraph
 from pyrogram import idle
 from telegram import InlineKeyboardMarkup, ParseMode, InlineKeyboardButton
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, run_async
 from bot import IGNORE_PENDING_REQUESTS, app, bot, botStartTime, dispatcher, updater, IS_VPS, IMAGE_URL, AUTHORIZED_CHATS
 from bot.helper.ext_utils import fs_utils
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
@@ -39,9 +39,11 @@ from bot.modules import (  # noqa
 )
 now=datetime.now(pytz.timezone('Asia/Kolkata'))
 
+@run_async
 def stats(update, context):
-    currentTime = get_readable_time(time.time() - botStartTime)
-    total, used, free = shutil.disk_usage(".")
+    currentTime = get_readable_time((time.time() - botStartTime))
+    current = now.strftime('%Y/%m/%d %I:%M:%S %p')
+    total, used, free = shutil.disk_usage('.')
     total = get_readable_file_size(total)
     used = get_readable_file_size(used)
     free = get_readable_file_size(free)
@@ -49,20 +51,21 @@ def stats(update, context):
     recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
-    stats = (
-        f"<b>Bot Uptime:</b> {currentTime}\n"
-        f"<b>Total disk space:</b> {total}\n"
-        f"<b>Used:</b> {used}  "
-        f"<b>Free:</b> {free}\n\n"
-        f"Data Usage\n<b>Upload:</b> {sent}\n"
-        f"<b>Down:</b> {recv}\n\n"
-        f"<b>CPU:</b> {cpuUsage}% "
-        f"<b>RAM:</b> {memory}% "
-        f"<b>Disk:</b> {disk}%"
-    )
-    sendMessage(stats, context.bot, update)
+    disk = psutil.disk_usage('/').percent
+    stats = f'<b>🤖Bot Uptime:</b> {currentTime}\n' \
+            f'<b>💪Start Time:</b> {current}\n' \
+            f'<b>Total disk space:</b> {total}\n' \
+            f'<b>📀Used:</b> {used}  ' \
+            f'<b>💿Free:</b> {free}\n\n' \
+            f'📊Data Usage📊\n<b>🔼Upload:</b> {sent}\n' \
+            f'<b>🔽Download:</b> {recv}\n\n' \
+            f'<b>🔳CPU:</b> {cpuUsage}% | ' \
+            f'<b>🔲RAM:</b> {memory}% | ' \
+            f'<b>🔵DISK:</b> {disk}%'
+    update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
+
+@run_async
 def start(update, context):
     start_string = f'''
 This bot can mirror all your links to Google drive!
@@ -71,19 +74,22 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN)
 
 
+@run_async
 def chat_list(update, context):
     chatlist =''
     chatlist += '\n'.join(str(id) for id in AUTHORIZED_CHATS)
     sendMessage(f'<b>Authorized List:</b>\n{chatlist}\n', context.bot, update)
 
 
+@run_async
 def repo(update, context):
-    buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Movies Group", "https://t.me/MOVIES_AND_SERIES_REQUESTING")
-    buttons.buildbutton("Updates Channel", "https://t.me/INDIAHDM0VIES")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
+    button = [
+    [InlineKeyboardButton("Movies Group", url=f"https://t.me/MOVIES_AND_SERIES_REQUESTING")],
+    [InlineKeyboardButton("Updates Channel", url=f"https://t.me/INDIAHDM0VIES")]]
+    reply_markup = InlineKeyboardMarkup(button)
     update.effective_message.reply_photo(IMAGE_URL, reply_markup=reply_markup)
     
+@run_async
 def restart(update, context):
     restart_message = sendMessage("Restarting, Please wait!", context.bot, update)
     # Save restart message ID and chat ID in order to edit it after restarting
@@ -92,18 +98,21 @@ def restart(update, context):
         f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
     fs_utils.clean_all()
     os.execl(executable, executable, "-m", "bot")
+    
 
-
+@run_async
 def ping(update, context):
     start_time = int(round(time.time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update)
     end_time = int(round(time.time() * 1000))
     editMessage(f"{end_time - start_time} ms", reply)
+    
 
-
+@run_async
 def log(update, context):
     sendLogFile(context.bot, update)
-
+    
+@run_async
 def bot_help(update, context):
     help_string_telegraph = f'''<br>
 <b>/{BotCommands.HelpCommand}</b>: To get this message
