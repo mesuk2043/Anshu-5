61643a57c78efeb55f484a426ca6fcc1af7f13fb
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-04-09 14:19:56 +0530

    Done

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,7 +66,7 @@ def stats(update, context):
 
 def start(update, context):
     start_string = f'''
-This bot can mirror all your links to Google drive!
+This bot can mirror all your links to Google drive & Telegram!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN)
